9097d74625b6da61b2d7c51ec434051d095264fb
szuev1980 <87873223+szuev1980@users.noreply.github.com>
2021-11-10 17:45:05 +0200

    Remove 1 from main

--- Recursion_base/Program.cs
+++ Recursion_base/Program.cs
@@ -6,7 +6,7 @@ namespace Recursion_base
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!1");
+            Console.WriteLine("Hello World!");
         }
     }
 }
